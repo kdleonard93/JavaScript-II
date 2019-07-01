02163719717dec0aef5dbb070b93a58454e4cbfd
Kyle Leonard <kdleonard@icloud.com>
2019-06-30 19:21:52 -0500

    Completed closure stretch problems
    However, i still have very little understanding on why this is the correct answer. I need to study this concept and practice more questions like these.

--- assignments/closure.js
+++ assignments/closure.js
@@ -12,16 +12,37 @@ console.log(returnClose());
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
+const counter = (count = 0) => increment = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  count++;
+  return count;
 };
+
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
+const counterFactory = () => objectFunction = {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  count: 0,
+  increment: () => {
+    objectFunction.count++
+    return objectFunction.count;
+  },
+  decrement: () => {
+    objectFunction.count--
+    return objectFunction.count;
+  }
 };
+const counter2 = counterFactory();
+
+console.log(counter2.increment());
+console.log(counter2.decrement());
